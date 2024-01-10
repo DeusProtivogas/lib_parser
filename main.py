538b674ccb327a0192ae3019d14eab52c58d27d6
kiril <kirill2689@gmail.com>
2024-01-10 15:02:01 +0300

    added connection security

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 import requests
 import argparse
 from bs4 import BeautifulSoup
@@ -8,18 +9,16 @@ from urllib.parse import urljoin
 from urllib.parse import urlparse
 
 
-def check_for_redirect(r):
-    if r.history:
+def check_for_redirect(response):
+    if response.history:
         raise requests.HTTPError
 
 
 def get_soup(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        check_for_redirect(response)
-    except requests.HTTPError:
-        return
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+
     soup = BeautifulSoup(response.text, 'lxml')
     return soup
 
@@ -35,12 +34,12 @@ def get_comments(soup):
     return [tag.find('span', class_="black").text for tag in tags]
 
 
-def get_genre(soup):
+def get_genres(soup):
     tags = soup.find('span', class_='d_book').find_all('a')
     return [tag.text for tag in tags]
 
 
-def get_image(soup, base_url="https://tululu.org/"):
+def get_image(soup, base_url):
     tag = soup.find('div', class_='bookimage')
     if tag:
         return urljoin(base_url, tag.find("img")['src'])
@@ -56,15 +55,14 @@ def download_txt(url, filename, folder='books/'):
         str: Путь до файла, куда сохранён текст.
     """
 
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        check_for_redirect(response)
-    except requests.HTTPError:
-        return
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
+
+    book_id = urlparse(url).query.split("=")[-1]
 
     Path(f"./{folder}").mkdir(parents=True, exist_ok=True)
-    name = f'{sanitize_filename(filename)}.txt'
+    name = f'{book_id}. {sanitize_filename(filename)}.txt'
     path = os.path.join(folder, name)
     with open(path, 'wb') as file:
         file.write(response.content)
@@ -72,12 +70,9 @@ def download_txt(url, filename, folder='books/'):
 
 
 def download_image(url, filename, folder='covers/'):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        check_for_redirect(response)
-    except requests.HTTPError:
-        return
+    response = requests.get(url)
+    response.raise_for_status()
+    check_for_redirect(response)
 
     Path(f"./{folder}").mkdir(parents=True, exist_ok=True)
     name = f'{filename}.{urlparse(url).path.split(".")[-1]}'
@@ -93,7 +88,7 @@ def parse_book_page(soup):
         "title": title,
         "author": author,
         "comments": get_comments(soup),
-        "genre": get_genre(soup),
+        "genres": get_genres(soup),
         "image": get_image(soup),
     }
 
@@ -108,16 +103,36 @@ def main():
     url_template = "https://tululu.org/b"
     url_image_source = "https://tululu.org/"
 
-    for id in range(int(args.start_id), int(args.end_id) + 1):
-        url = f"{url_template}{id}/"
-        url_txt = f"{url_txt_template}{id}"
-        soup = get_soup(url)
-        if not soup:
-            continue
-        title, author = get_title_and_author(soup)
-        image = get_image(soup, url_image_source)
-        download_image(image, id,)
-        download_txt(url_txt, title)
+    first_reconnection = True
+
+    book_id = int(args.start_id)
+    last_id = int(args.end_id)
+    while book_id <= last_id:
+        print(book_id)
+    # for book_id in range(int(args.start_id), int(args.end_id) + 1):
+        try:
+            url = f"{url_template}{book_id}/"
+            url_txt = f"{url_txt_template}{book_id}"
+            soup = get_soup(url)
+            if not soup:
+                continue
+            title, author = get_title_and_author(soup)
+            image = get_image(soup, url)
+            download_txt(url_txt, title)
+            download_image(image, book_id,)
+            book_id += 1
+        except requests.HTTPError:
+            print(f"Book {book_id} not found")
+            book_id += 1
+        except (requests.ConnectionError, requests.Timeout):
+            print("Connection has been interrupted, restarting...")
+            if first_reconnection:
+                first_reconnection = False
+                time.sleep(1)
+                continue
+            time.sleep(3)
+
+
 
 
 if __name__ == "__main__":
