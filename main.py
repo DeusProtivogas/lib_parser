055671976e77245649d23e2350a6c3d13bd71a47
kiril <kirill2689@gmail.com>
2024-01-10 15:45:52 +0300

    PEP8 fixes

--- main.py
+++ main.py
@@ -45,24 +45,13 @@ def get_image(soup, base_url):
         return urljoin(base_url, tag.find("img")['src'])
 
 
-def download_txt(url, filename, folder='books/'):
-    """Функция для скачивания текстовых файлов.
-    Args:
-        url (str): Cсылка на текст, который хочется скачать.
-        filename (str): Имя файла, с которым сохранять.
-        folder (str): Папка, куда сохранять.
-    Returns:
-        str: Путь до файла, куда сохранён текст.
-    """
-
-    response = requests.get(url)
+def download_txt(url, filename, params, folder='books/'):
+    response = requests.get(url, params)
     response.raise_for_status()
     check_for_redirect(response)
 
-    book_id = urlparse(url).query.split("=")[-1]
-
     Path(f"./{folder}").mkdir(parents=True, exist_ok=True)
-    name = f'{book_id}. {sanitize_filename(filename)}.txt'
+    name = f'{params["id"]}. {sanitize_filename(filename)}.txt'
     path = os.path.join(folder, name)
     with open(path, 'wb') as file:
         file.write(response.content)
@@ -82,47 +71,52 @@ def download_image(url, filename, folder='covers/'):
     return path
 
 
-def parse_book_page(soup):
+def parse_book_page(url):
+    soup = get_soup(url)
     title, author = get_title_and_author(soup)
     return {
         "title": title,
         "author": author,
         "comments": get_comments(soup),
         "genres": get_genres(soup),
-        "image": get_image(soup),
+        "image": get_image(soup, url),
     }
 
 
 def main():
     parser = argparse.ArgumentParser(description="Скачать книги и обложки")
-    parser.add_argument('--start_id', help='Начальный индекс (по умолчанию = 1)', default=1)
-    parser.add_argument('--end_id', help='Конечный индекс (по умолчанию = 10)', default=10)
+    parser.add_argument(
+        '--start_id',
+        help='Начальный индекс (по умолчанию = 1)',
+        default=1
+    )
+    parser.add_argument(
+        '--end_id',
+        help='Конечный индекс (по умолчанию = 10)',
+        default=10
+    )
     args = parser.parse_args()
 
-    url_txt_template = "https://tululu.org/txt.php?id="
+    url_txt_template = "https://tululu.org/txt.php"
     url_template = "https://tululu.org/b"
-    url_image_source = "https://tululu.org/"
 
     first_reconnection = True
-
     book_id = int(args.start_id)
     last_id = int(args.end_id)
     while book_id <= last_id:
-        print(book_id)
-    # for book_id in range(int(args.start_id), int(args.end_id) + 1):
         try:
+            params = {
+                "id": book_id,
+            }
             url = f"{url_template}{book_id}/"
-            url_txt = f"{url_txt_template}{book_id}"
-            soup = get_soup(url)
-            if not soup:
-                continue
+            soup = get_soup(url,)
             title, author = get_title_and_author(soup)
             image = get_image(soup, url)
-            download_txt(url_txt, title)
+            download_txt(url_txt_template, title, params)
             download_image(image, book_id,)
             book_id += 1
         except requests.HTTPError:
-            print(f"Book {book_id} not found")
+            print(f"Book with id {book_id} not found")
             book_id += 1
         except (requests.ConnectionError, requests.Timeout):
             print("Connection has been interrupted, restarting...")
@@ -131,8 +125,6 @@ def main():
                 time.sleep(1)
                 continue
             time.sleep(3)
-
-
 
 
 if __name__ == "__main__":
