1a42a988d6b79e8f4be671ad1f84fe459060c731
kiril <kirill2689@gmail.com>
2024-01-30 17:07:23 +0300

    added more commands

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -1,5 +1,7 @@
 import os
+import sys
 import json
+import argparse
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
@@ -9,16 +11,17 @@ from pathvalidate import sanitize_filename
 
 
 def check_for_redirect(response):
-    # if response.history:
-    #     raise requests.HTTPError
-    pass
+    if response.history:
+        raise requests.HTTPError
+    # pass
 
 def get_soup(url):
     # print("SOUP ", url)
     response = requests.get(url)
+    # print(response)
     response.raise_for_status()
-    # print(response.history)
-    check_for_redirect(response)
+    # print(url, response.history)
+    # check_for_redirect(response)
     # print("Soup", response)
 
     soup = BeautifulSoup(response.text, 'lxml')
@@ -27,32 +30,30 @@ def get_soup(url):
 
 
 def get_title_and_author(soup):
-    # print(soup)
-    # print(soup.find(id= "content"))
-    tag = soup.find('body').find('h1')
-    # print("TAG ",tag)
+    selector = "body h1"
+    tag = soup.select_one(selector)
     title, author = map(str.strip, tag.text.split("::"))
     return title, author
 
 
 def get_comments(soup):
-    tags = soup.find_all('div', class_='texts')
-    return [tag.find('span', class_="black").text for tag in tags]
+    selector = "div.texts span.black"
+    return [tag.text for tag in soup.select(selector)]
 
 
 def get_genres(soup):
-    tags = soup.find('span', class_='d_book').find_all('a')
-    return [tag.text for tag in tags]
+    selector = "span.d_book a"
+    return [tag.text for tag in soup.select(selector)]
 
 
 def get_image(soup, base_url):
-    tag = soup.find('div', class_='bookimage')
+    selector = "div.bookimage"
+    tag = soup.select_one(selector)
     if tag:
-        return urljoin(base_url, tag.find("img")['src'])
-
+        return urljoin(base_url, tag.select_one("img")['src'])
 
 def parse_book_page(soup, url):
-    print(url)
+    # print(url)
     title, author = get_title_and_author(soup)
     return {
         "title": title,
@@ -63,97 +64,139 @@ def parse_book_page(soup, url):
     }
 
 
-def download_txt(url, filename, params, folder='books/'):
+def download_txt(url, filename, params, dest_folder, folder='books/'):
     response = requests.get(url, params)
-    print(response)
+    # print(url, params)
+    # print(response.content)
     response.raise_for_status()
     check_for_redirect(response)
 
-    Path(f"./{folder}").mkdir(parents=True, exist_ok=True)
+    Path(f"{dest_folder}/{folder}").mkdir(parents=True, exist_ok=True)
     name = f'{params["id"]}. {sanitize_filename(filename)}.txt'
-    path = os.path.join(folder, name)
+    path = os.path.join(dest_folder, folder, name)
     with open(path, 'wb') as file:
         file.write(response.content)
     return path
 
 
-def download_image(url, filename, folder='covers/'):
+def download_image(url, filename, dest_folder, folder='covers/'):
     response = requests.get(url)
     response.raise_for_status()
     check_for_redirect(response)
 
-    Path(f"./{folder}").mkdir(parents=True, exist_ok=True)
+    Path(f"{dest_folder}/{folder}").mkdir(parents=True, exist_ok=True)
     name = f'{filename}.{urlparse(url).path.split(".")[-1]}'
-    path = os.path.join(folder, name)
+    path = os.path.join(dest_folder, folder, name)
     with open(path, 'wb') as file:
         file.write(response.content)
     return path
 
 
-# def get_comments(soup):
-#     tags = soup.find_all('div', class_='texts')
-#     return [tag.find('span', class_="black").text for tag in tags]
-#
-#
-# def get_genres(soup):
-#     tags = soup.find('span', class_='d_book').find_all('a')
-#     return [tag.text for tag in tags]
-
-
 def main():
+    parser = argparse.ArgumentParser(description="Скачать книги и обложки")
+    parser.add_argument(
+        '--skip_imgs',
+        help='Не скачивать картинки (по умолчанию = False)',
+        default=False
+    )
+    parser.add_argument(
+        '--skip_txt',
+        help='Не скачивать текста (по умолчанию = False)',
+        default=False
+    )
+    parser.add_argument(
+        '--dest_folder',
+        help='Путь к каталогу с результатами (по умолчанию = 1)',
+        default="./"
+    )
+    parser.add_argument(
+        '--start_page',
+        help='Начальная страница (по умолчанию = 1)',
+        default=1
+    )
+    parser.add_argument(
+        '--end_page',
+        help='Конечная страница (по умолчанию - пока не закончатся страницы)',
+        default=sys.maxsize
+    )
+    args = parser.parse_args()
+
     url_book = "https://tululu.org/b"
     url_all_books_template = "https://tululu.org/l55/"
     url_txt_template = "https://tululu.org/txt.php"
     # https://tululu.org/txt.php?id=b239
     # https: // tululu.org / txt.php?id = 239
 
-    books = []
     books_short_info = []
-    last_page = 1
-    for page in range(1, last_page + 1):
-        soup = get_soup(urljoin(url_all_books_template, str(page)))
-        books.extend( soup.find_all('table', class_='d_book') )
+    # last_page = 1
 
-    for count, book in enumerate(books):
+    # last_page =
+
+    first_reconnection = True
+    dest_folder = args.dest_folder
+    skip_imgs = bool(args.skip_imgs)
+    skip_txt = bool(args.skip_txt)
+    start_page = int(args.start_page)
+    last_page = int(args.end_page)
+    for page in range(start_page, last_page + 1):
+        # print(page)
         try:
-            # print(book)
-            book_id = book.find('a')['href'].strip('/').strip('b')
-            params = {
-                "id": book_id,
-            }
-            # url = f"{url_template}{book_id}/"
-            url = f"{url_book}{book_id}"
-            # print("NOT SOUP ", url_book)
-            soup = get_soup(url,)
+            # response =
+            # print(urljoin(url_all_books_template, str(page)))
+            soup = get_soup(urljoin(url_all_books_template, str(page)))
             # print(soup)
-            print("TEST")
-            book = parse_book_page(soup, url)
+            books = soup.select("table.d_book")
+            if not books:
+                print(f"Ran out of pages")
+                break
 
-            title, author = book["title"], book["author"]
-            image = book["image"]
-            book_path = download_txt(url_txt_template, title, params)
-            img_path = download_image(image, book_id,)
-            books_short_info.append({
-                "title": title,
-                "author": author,
-                "img_src": img_path,
-                "book_path": book_path,
-                "comments": get_comments(soup),
-                "genres": get_genres(soup),
-            })
-            # book_id += 1
+            for count, book in enumerate(books):
+                # print(count)
+                try:
+                    # print(book)
+                    book_id = book.select_one('a')['href'].strip('/').strip('b')
+                    params = {
+                        "id": book_id,
+                    }
+                    url = f"{url_book}{book_id}"
+                    # print(url)
+                    soup = get_soup(url,)
+                    # print(soup)
+                    book = parse_book_page(soup, url)
+
+                    title, author = book["title"], book["author"]
+                    image = book["image"]
+                    book_path = None
+                    if not skip_txt:
+                        book_path = download_txt(url_txt_template, title, params, dest_folder)
+                    img_path = None
+                    if not skip_imgs:
+                        img_path = download_image(image, book_id, dest_folder)
+                    books_short_info.append({
+                        "title": title,
+                        "author": author,
+                        "img_src": img_path,
+                        "book_path": book_path,
+                        "comments": get_comments(soup),
+                        "genres": get_genres(soup),
+                    })
+                    # book_id += 1
+                except requests.HTTPError:
+                    print(f"Book with id {book_id} not found")
+                    # book_id += 1
+                # url = f"{url_template}{book_id}/"
+                # print(url)
+                if count >= 1:
+                    break
         except requests.HTTPError:
-            print(f"Book with id {book_id} not found")
-            # book_id += 1
-        # url = f"{url_template}{book_id}/"
-        # print(url)
-        if count >= 5:
+            print(f"Ran out of pages")
             break
-    print(books_short_info)
 
     # books_short_info_json = json.dump(books_short_info, ensure_ascii=False).encode('utf8')
 
-    with open("books_short_info.json", "w", encoding='utf8') as my_file:
+
+
+    with open(os.path.join(dest_folder, "books_short_info.json"), "w", encoding='utf8') as my_file:
         json.dump(books_short_info, my_file, ensure_ascii=False)
 
     # print(len(books))
