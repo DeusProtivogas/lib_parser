eff58d4bc8e8f584d8d715907bfbb77eed36cb24
kiril <kirill2689@gmail.com>
2024-02-12 23:54:45 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -2,8 +2,27 @@ import os
 import json
 
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-
+from livereload import Server, shell
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+def on_reload():
+    dest_folder = "temp_folder"
+    with open(os.path.join(dest_folder, "books.json"), "r", encoding='utf8') as file:
+        # print(file)
+        books_json = file.read()
+
+    books = json.loads(books_json)
+
+    for book in books:
+        replace_slash(book)
+
+    template = env.get_template('index_template.html')
+    rendered_page = template.render(
+        books=books,
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
 def replace_slash(book):
     book["img_src"] = book["img_src"].replace("\\", "/")
@@ -13,24 +32,28 @@ env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
+#
+# dest_folder = "temp_folder"
+# with open(os.path.join(dest_folder, "books.json"), "r", encoding='utf8') as file:
+#     # print(file)
+#     books_json = file.read()
+#
+# books = json.loads(books_json)
+#
+# for book in books:
+#     replace_slash(book)
+#
+# template = env.get_template('index_template.html')
+# rendered_page = template.render(
+#     books=books,
+# )
+#
+# with open('index.html', 'w', encoding="utf8") as file:
+#     file.write(rendered_page)
 
-dest_folder = "temp_folder"
-with open(os.path.join(dest_folder, "books.json"), "r", encoding='utf8') as file:
-    # print(file)
-    books_json = file.read()
+on_reload()
 
-books = json.loads(books_json)
+server = Server()
+server.watch('index_template.html', on_reload)
+server.serve(root='.')
 
-for book in books:
-    replace_slash(book)
-
-template = env.get_template('index_template.html')
-rendered_page = template.render(
-    books=books,
-)
-
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
-
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
