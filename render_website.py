ff8a521bf489446fdda2cca3ad4408cb5ae2ad17
kiril <kirill2689@gmail.com>
2024-02-13 21:38:40 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -4,10 +4,15 @@ import json
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from livereload import Server, shell
 from more_itertools import chunked
+from pathlib import Path
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 def on_reload():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
     dest_folder = "temp_folder"
     with open(os.path.join(dest_folder, "books.json"), "r", encoding='utf8') as file:
         # print(file)
@@ -18,47 +23,34 @@ def on_reload():
     for book in books:
         replace_slash(book)
 
-    books = list(chunked(books, 2))
+    books = list( chunked(list(chunked(books, 2)), 10 ) )
     # print(books)
 
     template = env.get_template('index_template.html')
-    rendered_page = template.render(
-        books=books,
-    )
 
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    for counter, books_portion in enumerate(books):
+        rendered_page = template.render(
+            books=books_portion,
+        )
+
+        Path(os.path.join("pages")).mkdir(parents=True, exist_ok=True)
+        with open(os.path.join("pages", f'index{counter}.html'), 'w', encoding="utf8") as file:
+            file.write(rendered_page)
+
 
 def replace_slash(book):
     book["img_src"] = book["img_src"].replace("\\", "/")
     book["book_path"] = book["book_path"].replace("\\", "/")
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-#
-# dest_folder = "temp_folder"
-# with open(os.path.join(dest_folder, "books.json"), "r", encoding='utf8') as file:
-#     # print(file)
-#     books_json = file.read()
-#
-# books = json.loads(books_json)
-#
-# for book in books:
-#     replace_slash(book)
-#
-# template = env.get_template('index_template.html')
-# rendered_page = template.render(
-#     books=books,
-# )
-#
-# with open('index.html', 'w', encoding="utf8") as file:
-#     file.write(rendered_page)
 
-on_reload()
+def main():
 
-server = Server()
-server.watch('index_template.html', on_reload)
-server.serve(root='.')
+    on_reload()
 
+    server = Server()
+    server.watch('index_template.html', on_reload)
+    server.serve(root='.')
+
+
+if __name__ == "__main__":
+    main()
